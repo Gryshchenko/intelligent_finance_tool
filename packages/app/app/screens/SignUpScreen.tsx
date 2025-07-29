546f8150d083776c47655494f007b7742c9efb99
VG <->
2025-07-29 21:58:50 +0200

    add monorep

---
+++ packages/app/app/screens/SignUpScreen.tsx
@@ -0,0 +1,170 @@
+import { ComponentType, FC, useEffect, useMemo, useRef, useState } from "react"
+// eslint-disable-next-line no-restricted-imports
+import { TextInput, TextStyle, ViewStyle } from "react-native"
+
+import { Button } from "@/components/Button"
+import { PressableIcon } from "@/components/Icon"
+import { Screen } from "@/components/Screen"
+import { Text } from "@/components/Text"
+import { TextField, type TextFieldAccessoryProps } from "@/components/TextField"
+import { useAuth } from "@/context/AuthContext"
+import type { AppStackScreenProps } from "@/navigators/AppNavigator"
+import { useAppTheme } from "@/theme/context"
+import type { ThemedStyle } from "@/theme/types"
+
+interface SignUpScreenProps extends AppStackScreenProps<"SignUp"> {}
+
+export const SignUpScreen: FC<SignUpScreenProps> = () => {
+  const authPasswordInput = useRef<TextInput>(null)
+
+  const [authPassword, setAuthPassword] = useState<{ origin: string; repeat: string }>({
+    origin: "",
+    repeat: "",
+  })
+  const [name, setName] = useState<string>("")
+  const [isAuthPasswordHidden, setIsAuthPasswordHidden] = useState(true)
+  const [isSubmitted, setIsSubmitted] = useState(false)
+  const [attemptsCount, setAttemptsCount] = useState(0)
+  const { authEmail, setAuthEmail, setAuthToken, validationError } = useAuth()
+
+  const {
+    themed,
+    theme: { colors },
+  } = useAppTheme()
+
+  useEffect(() => {
+    // Here is where you could fetch credentials from keychain or storage
+    // and pre-fill the form fields.
+    // setAuthEmail("ignite@infinite.red")
+    // setAuthPassword("ign1teIsAwes0m3")
+  }, [setAuthEmail])
+
+  const error = isSubmitted ? validationError : ""
+
+  function signUp() {
+    setIsSubmitted(true)
+    setAttemptsCount(attemptsCount + 1)
+
+    if (validationError) return
+
+    // Make a request to your server to get an authentication token.
+    // If successful, reset the fields and set the token.
+    setIsSubmitted(false)
+    setAuthPassword({ repeat: "", origin: "" })
+    setAuthEmail("")
+
+    // We'll mock this with a fake token.
+    setAuthToken(String(Date.now()))
+  }
+
+  const PasswordRightAccessory: ComponentType<TextFieldAccessoryProps> = useMemo(
+    () =>
+      function PasswordRightAccessory(props: TextFieldAccessoryProps) {
+        return (
+          <PressableIcon
+            icon={isAuthPasswordHidden ? "view" : "hidden"}
+            color={colors.palette.neutral800}
+            containerStyle={props.style}
+            size={20}
+            onPress={() => setIsAuthPasswordHidden(!isAuthPasswordHidden)}
+          />
+        )
+      },
+    [isAuthPasswordHidden, colors.palette.neutral800],
+  )
+
+  return (
+    <Screen
+      preset="auto"
+      contentContainerStyle={themed($screenContentContainer)}
+      safeAreaEdges={["top", "bottom"]}
+    >
+      <Text testID="signUp-heading" tx="common:signUp" preset="heading" style={themed($logIn)} />
+      <Text tx="signUpScreen:enterDetails" preset="subheading" style={themed($enterDetails)} />
+
+      <TextField
+        value={name}
+        onChangeText={setName}
+        containerStyle={themed($textField)}
+        autoCapitalize="none"
+        autoCorrect={false}
+        keyboardType="default"
+        labelTx="common:nameFieldLabel"
+        placeholderTx="common:nameFieldPlaceholder"
+        helper={error}
+        status={error ? "error" : undefined}
+        onSubmitEditing={() => authPasswordInput.current?.focus()}
+      />
+      <TextField
+        value={authEmail}
+        onChangeText={setAuthEmail}
+        containerStyle={themed($textField)}
+        autoCapitalize="none"
+        autoComplete="email"
+        autoCorrect={false}
+        keyboardType="email-address"
+        labelTx="common:emailFieldLabel"
+        placeholderTx="common:emailFieldPlaceholder"
+        helper={error}
+        status={error ? "error" : undefined}
+        onSubmitEditing={() => authPasswordInput.current?.focus()}
+      />
+
+      <TextField
+        ref={authPasswordInput}
+        value={authPassword.origin}
+        onChangeText={(pass) => setAuthPassword((prev) => ({ ...prev, origin: pass }))}
+        containerStyle={themed($textField)}
+        autoCapitalize="none"
+        autoComplete="password"
+        autoCorrect={false}
+        secureTextEntry={isAuthPasswordHidden}
+        labelTx="common:passwordFieldLabel"
+        placeholderTx="common:passwordFieldPlaceholder"
+        RightAccessory={PasswordRightAccessory}
+      />
+      <TextField
+        ref={authPasswordInput}
+        value={authPassword.repeat}
+        onChangeText={(pass) => setAuthPassword((prev) => ({ ...prev, repeat: pass }))}
+        containerStyle={themed($textField)}
+        autoCapitalize="none"
+        autoComplete="password"
+        autoCorrect={false}
+        secureTextEntry={isAuthPasswordHidden}
+        labelTx="signUpScreen:repeatPasswordFieldLabel"
+        placeholderTx="signUpScreen:repeatPasswordFieldPlaceholder"
+        RightAccessory={PasswordRightAccessory}
+      />
+
+      <Button
+        testID="signUp-button"
+        tx="signUpScreen:tapToLogIn"
+        style={themed($tapButton)}
+        preset="reversed"
+        onPress={signUp}
+      />
+    </Screen>
+  )
+}
+
+const $screenContentContainer: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  paddingVertical: spacing.xxl,
+  paddingHorizontal: spacing.lg,
+})
+
+const $logIn: ThemedStyle<TextStyle> = ({ spacing }) => ({
+  marginBottom: spacing.sm,
+})
+
+const $enterDetails: ThemedStyle<TextStyle> = ({ spacing }) => ({
+  marginBottom: spacing.lg,
+})
+
+const $textField: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  marginBottom: spacing.lg,
+})
+
+const $tapButton: ThemedStyle<ViewStyle> = ({ spacing }) => ({
+  marginTop: spacing.xs,
+})
